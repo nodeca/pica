078cb99bb1cdf081d04d3d83b4d034d5c6e8653b
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-09-25 11:29:10 +0400

    Updated profiling helper

--- benchmark/profile.js
+++ benchmark/profile.js
@@ -20,11 +20,17 @@ ctx.drawImage(image, 0, 0, image.width, image.height);
 
 var data = ctx.getImageData(0, 0, image.width, image.height).data;
 
-pica.resizeBuffer({
-  src: data,
-  width: image.width,
-  hieght: image.height,
-  toWidth: 300,
-  toHeight: 225,
-  quality: 3
-}, function() {});
+pica.WW = false;
+
+for (var i = 0; i < 10; i++) {
+  /*eslint no-loop-func:0*/
+  pica.resizeBuffer({
+    src: data,
+    width: image.width,
+    height: image.height,
+    toWidth: 300,
+    toHeight: 225,
+    quality: 3
+  }, function(/*__, result*/) {
+  });
+}
