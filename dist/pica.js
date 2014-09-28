4fe20ac92a9cc7919f0a645ab9645a24a766e271
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-09-28 23:50:21 +0400

    Browser files rebuild

--- dist/pica.js
+++ dist/pica.js
@@ -44,7 +44,9 @@ function resizeBuffer(options, callback) {
     toWidth:  options.toWidth|0,
     toHeight: options.toHeight|0,
     quality:  options.quality,
-    alpha:    options.alpha
+    alpha:    options.alpha,
+    unsharpAmount:    options.unsharpAmount,
+    unsharpThreshold: options.unsharpThreshold
   };
 
   if (WORKER && exports.WW) {
@@ -108,7 +110,9 @@ function resizeCanvas(from, to, options, callback) {
     toWidth:  to.width,
     toHeight: to.height,
     quality:  options.quality,
-    alpha:    options.alpha
+    alpha:    options.alpha,
+    unsharpAmount:    options.unsharpAmount,
+    unsharpThreshold: options.unsharpThreshold
   };
 
   resizeBuffer(_opts, function (err/*, output*/) {
@@ -127,10 +131,13 @@ exports.resizeBuffer = resizeBuffer;
 exports.resizeCanvas = resizeCanvas;
 exports.WW = WORKER;
 
-},{"./lib/resize":2,"./lib/resize_worker":3,"webworkify":4}],1:[function(require,module,exports){
+},{"./lib/resize":3,"./lib/resize_worker":4,"webworkify":5}],1:[function(require,module,exports){
 // High speed resize with tuneable speed/quality ratio
 
 'use strict';
+
+
+var unsharp = require('./unsharp');
 
 
 // Precision of fixed FP values
@@ -394,6 +401,8 @@ function resize(options) {
   var dest  = options.dest || new Uint8Array(destW * destH * 4);
   var quality = options.quality === undefined ? 3 : options.quality;
   var alpha = options.alpha || false;
+  var unsharpAmount = options.unsharpAmount === undefined ? 0 : (options.unsharpAmount|0);
+  var unsharpThreshold = options.unsharpThreshold === undefined ? 0 : (options.unsharpThreshold|0);
 
   if (srcW < 1 || srcH < 1 || destW < 1 || destH < 1) { return []; }
 
@@ -405,13 +414,150 @@ function resize(options) {
   convolveHorizontally(src, tmp, srcW, srcH, destW, destH, filtersX);
   convolveVertically(tmp, dest, destW, srcH, destW, destH, filtersY, alpha);
 
+  if (unsharpAmount) {
+    unsharp(dest, destW, destH, unsharpAmount, 1.0, unsharpThreshold);
+  }
+
   return dest;
 }
 
 
 module.exports = resize;
 
-},{}],2:[function(require,module,exports){
+},{"./unsharp":2}],2:[function(require,module,exports){
+// Unsharp mask filter
+//
+// http://stackoverflow.com/a/23322820/1031804
+// USM(O) = O + (2 * (Amount / 100) * (O - GB))
+// GB - gaussial blur.
+//
+// brightness = 0.299*R + 0.587*G + 0.114*B
+// http://stackoverflow.com/a/596243/1031804
+//
+// To simplify math, normalize brighness mutipliers to 2^16:
+//
+// brightness = (19595*R + 38470*G + 7471*B) / 65536
+
+'use strict';
+
+
+function clampTo8(i) { return i < 0 ? 0 : (i > 255 ? 255 : i); }
+
+// Convert image to greyscale, 32bits FP result (16.16)
+//
+function greyscale(src, srcW, srcH) {
+  var size = srcW * srcH;
+  var result = new Uint32Array(size); // We don't use sign, but that helps to JIT
+  var i, srcPtr;
+
+  for (i = 0, srcPtr = 0; i < size; i++) {
+    result[i] = (src[srcPtr++] * 19595 // red
+              + src[srcPtr++] * 38470 // green
+              + src[srcPtr] * 7471) >>> 8;   // blue
+    srcPtr += 2;
+  }
+
+  return result;
+}
+
+var _blurKernel = new Uint8Array([
+  1, 2, 1,
+  2, 4, 2,
+  1, 2, 1
+]);
+
+var _bkWidth = Math.floor(Math.sqrt(_blurKernel.length));
+var _bkHalf = Math.floor(_bkWidth / 2);
+var _bkWsum = 0;
+for (var wc=0; wc < _blurKernel.length; wc++) { _bkWsum += _blurKernel[wc]; }
+
+
+function blur(gs, x, y, srcW, srcH) {
+  var bx, by, sx, sy, w, wsum, br;
+  var bPtr = 0;
+  var blurKernel = _blurKernel;
+  var bkHalf = _bkHalf;
+
+  wsum = 0; // weight sum to normalize result
+  br   = 0;
+
+  if (x >= bkHalf && y >= bkHalf && x + bkHalf < srcW && y + bkHalf < srcH) {
+    for (by = 0; by < 3; by++) {
+      for (bx = 0; bx < 3; bx++) {
+        sx = x + bx - bkHalf;
+        sy = y + by - bkHalf;
+
+        br += gs[sx + sy * srcW] * blurKernel[bPtr++];
+      }
+    }
+    return (br - (br % _bkWsum)) / _bkWsum;
+  }
+
+  for (by = 0; by < 3; by++) {
+    for (bx = 0; bx < 3; bx++) {
+      sx = x + bx - bkHalf;
+      sy = y + by - bkHalf;
+
+      if (sx >= 0 && sx < srcW && sy >= 0 && sy < srcH) {
+        w = blurKernel[bPtr];
+        wsum += w;
+        br += gs[sx + sy * srcW] * w;
+      }
+      bPtr++;
+    }
+  }
+  return (br - (br % wsum)) / wsum;
+}
+
+// Apply unsharp mask to src
+//
+// NOTE: radius is ignored to simplify gaussian blur calculation
+// on practice we need radius 0.3..2.0. Use 1.0 now.
+//
+function unsharp(src, srcW, srcH, amount, radius, threshold) {
+  var x, y, c, diff = 0, corr, srcPtr;
+
+  // Normalized delta multiplier. Expect that:
+  var AMOUNT_NORM = Math.floor(amount * 256 / 50);
+
+  // Convert to grayscale:
+  //
+  // - prevent color drift
+  // - speedup blur calc
+  //
+  var gs = greyscale(src, srcW, srcH);
+  var fpThreshold = threshold << 8;
+  var gsPtr = 0;
+
+  for (y = 0; y < srcH; y++) {
+    for (x = 0; x < srcW; x++) {
+
+      // calculate brightness blur, difference & update source buffer
+
+      diff = (gs[gsPtr++] - blur(gs, x, y, srcW, srcH))|0;
+
+      // Update source image if thresold exceeded
+      if (Math.abs(diff) > fpThreshold) {
+        // Calculate correction multiplier
+        corr = 65536 + ((diff * AMOUNT_NORM) >> 8);
+        srcPtr = (x + y * srcW) * 4;
+
+        c = src[srcPtr];
+        src[srcPtr++] = clampTo8((c * corr) >> 16);
+        c = src[srcPtr];
+        src[srcPtr++] = clampTo8((c * corr) >> 16);
+        c = src[srcPtr];
+        src[srcPtr] = clampTo8((c * corr) >> 16);
+      }
+
+    } // end row
+  } // end column
+}
+
+
+module.exports = unsharp;
+
+},{}],3:[function(require,module,exports){
 // Proxy to simplify split between webworker/plain calls
 'use strict';
 
@@ -423,7 +569,7 @@ module.exports = function (options, callback) {
   callback(null, output);
 };
 
-},{"./pure/resize":1}],3:[function(require,module,exports){
+},{"./pure/resize":1}],4:[function(require,module,exports){
 // Web Worker wrapper for image resize function
 
 'use strict';
@@ -443,7 +589,7 @@ module.exports = function(self) {
   };
 };
 
-},{"./resize":2}],4:[function(require,module,exports){
+},{"./resize":3}],5:[function(require,module,exports){
 var bundleFn = arguments[3];
 var sources = arguments[4];
 var cache = arguments[5];
