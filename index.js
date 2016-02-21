f25589f773f236c388d13b069e5092b250b1d538
Alexander Rodin <rodin.alexander@gmail.com>
2016-02-22 00:12:25 +0300

    Moved tiling control to lower level

--- index.js
+++ index.js
@@ -34,171 +34,14 @@ try {
   __cvs = null;
 }
 
-var createCanvas  = require('./lib/util/create_canvas.js');
-
-var ResizerJS     = require('./lib/resizer_js');
-var ResizerJSWW   = require('./lib/resizer_js_ww');
-var ResizerWebgl  = require('./lib/resizer_webgl');
+var resize_js     = require('./lib/resize_js');
+var resize_js_ww  = require('./lib/resize_js_ww');
+var resize_webgl  = require('./lib/resize_webgl');
 
 ////////////////////////////////////////////////////////////////////////////////
 // Helpers
 function _class(obj) { return Object.prototype.toString.call(obj); }
 function isFunction(obj) { return _class(obj) === '[object Function]'; }
-
-/////////////////////////////////////////////////////////////////////////////////
-// Making tiles
-
-var SRC_TILE_SIZE = 512;
-var DEST_TILE_BORDER = 3;
-
-function createRegions(fromWidth, fromHeight, toWidth, toHeight) {
-  var scaleX = toWidth / fromWidth;
-  var scaleY = toHeight / fromHeight;
-
-  var innerTileWidth = Math.floor(SRC_TILE_SIZE * scaleX) - 2 * DEST_TILE_BORDER;
-  var innerTileHeight = Math.floor(SRC_TILE_SIZE * scaleY) - 2 * DEST_TILE_BORDER;
-
-  var x, y;
-  var innerX, innerY, toTileWidth, toTileHeight;
-  var tiles = [];
-  var tile;
-
-  // we go top-to-down instead of left-to-right to make image displayed from top to
-  // doesn in the browser
-  for (innerY = 0; innerY < toHeight; innerY += innerTileHeight) {
-    for (innerX = 0; innerX < toWidth; innerX += innerTileWidth) {
-      x = innerX - DEST_TILE_BORDER;
-      if (x < 0) { x = 0; }
-      toTileWidth = innerX + innerTileWidth + DEST_TILE_BORDER - x;
-      if (x + toTileWidth >= toWidth) {
-        toTileWidth = toWidth - x;
-      }
-
-      y = innerY - DEST_TILE_BORDER;
-      if (y < 0) { y = 0; }
-      toTileHeight = innerY + innerTileHeight + DEST_TILE_BORDER - y;
-      if (y + toTileHeight >= toHeight) {
-        toTileHeight = toHeight - y;
-      }
-
-      tile = {
-        toX: x,
-        toY: y,
-        toWidth: toTileWidth,
-        toHeight: toTileHeight,
-
-        toInnerX: innerX,
-        toInnerY: innerY,
-        toInnerWidth: innerTileWidth,
-        toInnerHeight: innerTileHeight,
-
-        offsetX: x / scaleX - Math.floor(x / scaleX),
-        offsetY: y / scaleY - Math.floor(y / scaleY),
-        scaleX: scaleX,
-        scaleY: scaleY,
-
-        x: Math.floor(x / scaleX),
-        y: Math.floor(y / scaleY),
-        width: Math.ceil(toTileWidth / scaleX),
-        height: Math.ceil(toTileHeight / scaleY)
-      };
-
-      tiles.push(tile);
-    }
-  }
-
-  return tiles;
-}
-
-function eachLimit(list, limit, iterator, callback) {
-  if (list.length === 0) {
-    callback();
-  }
-
-  var current = 0;
-  var failed = false;
-
-  var next = function (err) {
-    if (err) {
-      if (!failed) {
-        failed = true;
-        callback(err);
-      }
-      return;
-    }
-
-    if (current < list.length) {
-      iterator(list[current++], next);
-    } else {
-      callback();
-    }
-  };
-
-  for (current = 0; current < limit && current < list.length; current++) {
-    iterator(list[current], next);
-  }
-}
-
-function resizeTiled (from, to, options, resizer, callback) {
-  var regions = createRegions(from.width, from.height, to.width, to.height);
-  var toCtx = to.getContext('2d');
-
-  var canvasPool  = [];
-  var i, tileData;
-
-  for (i = 0; i < resizer.concurrency; i++) {
-    tileData = {
-      src: createCanvas(),
-      dest: createCanvas()
-    };
-    tileData.src.width = SRC_TILE_SIZE;
-    tileData.src.height = SRC_TILE_SIZE;
-    tileData.dest.width = Math.floor(SRC_TILE_SIZE * to.width / from.width);
-    tileData.dest.height = Math.floor(SRC_TILE_SIZE * to.height / from.height);
-
-    canvasPool.push(tileData);
-  }
-
-  eachLimit(regions, resizer.concurrency, function (tile, next) {
-    var canvases = canvasPool.pop();
-
-    canvases.src.getContext('2d').drawImage(from,
-      tile.x, tile.y, tile.width, tile.height,
-      0, 0, tile.width, tile.height);
-
-    var _opts = {
-      width:    tile.width,
-      height:   tile.height,
-      toWidth:  tile.toWidth,
-      toHeight: tile.toHeight,
-      scaleX:   tile.scaleX,
-      scaleY:   tile.scaleY,
-      offsetX:  tile.offsetX,
-      offsetY:  tile.offsetY,
-      quality:  options.quality,
-      alpha:    options.alpha,
-      unsharpAmount: options.unsharpAmount,
-      unsharpRadius: options.unsharpRadius,
-      unsharpThreshold: options.unsharpThreshold
-    };
-
-    resizer.resize(canvases.src, canvases.dest, _opts, function (err) {
-      if (!err) {
-        toCtx.drawImage(canvases.dest,
-          tile.toInnerX - tile.toX, tile.toInnerY - tile.toY,
-          tile.toInnerWidth, tile.toInnerHeight,
-          tile.toInnerX, tile.toInnerY,
-          tile.toInnerWidth, tile.toInnerHeight);
-      }
-
-      canvasPool.push(canvases);
-      next(err);
-    });
-  }, function (err) {
-    resizer.cleanup();
-    callback(err);
-  });
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 // API methods
@@ -221,7 +64,7 @@ function resizeCanvas(from, to, options, callback) {
   if (WEBGL && exports.WEBGL) {
     exports.debug('Resize canvas with WebGL');
 
-    return resizeTiled(from, to, options, new ResizerWebgl(), function (err) {
+    return resize_webgl(from, to, options, function (err) {
       if (err) {
         exports.debug('WebGL resize failed, do fallback and cancel next attempts');
         exports.debug(err);
@@ -239,13 +82,13 @@ function resizeCanvas(from, to, options, callback) {
   if (WORKER && exports.WW) {
     exports.debug('Resize buffer in WebWorker');
 
-    return resizeTiled(from, to, options, new ResizerJSWW(), callback);
+    return resize_js_ww(from, to, options, callback);
   }
 
   // Fallback to sync call, if WebWorkers not available
   exports.debug('Resize buffer sync (freeze event loop)');
 
-  return resizeTiled(from, to, options, new ResizerJS(), callback);
+  return resize_js(from, to, options, callback);
 }
 
 
