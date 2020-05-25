8a145aab201590bb6fadb7e44282db6a52b6f2a2
Vitaly Puzrin <vitaly@rcdesign.ru>
2020-05-25 17:37:57 +0300

    browser files rebuild

--- dist/pica.js
+++ dist/pica.js
@@ -1,4 +1,4 @@
-/* pica 5.1.1 nodeca/pica */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.pica = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+/* pica 5.2.0 nodeca/pica */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.pica = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // Collection of math functions
 //
 // 1. Combine components together
@@ -741,7 +741,7 @@ module.exports.isCanvas = function isCanvas(element) {
   var cname = objClass(element);
   return cname === '[object HTMLCanvasElement]'
   /* browser */
-  || cname === '[object Canvas]'
+  || cname === '[object OffscreenCanvas]' || cname === '[object Canvas]'
   /* node-canvas */
   ;
 };
@@ -2192,6 +2192,14 @@ Pica.prototype.toBlob = function (canvas, mimeType, quality) {
       canvas.toBlob(function (blob) {
         return resolve(blob);
       }, mimeType, quality);
+      return;
+    }
+
+    if (canvas.convertToBlob) {
+      resolve(canvas.convertToBlob({
+        type: mimeType,
+        quality: quality
+      }));
       return;
     } // Fallback for old browsers
 
