1deca6ea66bb7d875e16000964ccb6c1c11b9271
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-05-10 07:25:22 +0300

    eslint => neostandard

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import neostandard from 'neostandard'
+import globals from 'globals'
+
+export default [
+  ...neostandard({
+    env: ['browser', 'node'],
+    ignores: [
+      'benchmark/implementations/**',
+      'demo/**',
+      'dist/**'
+    ]
+  }),
+
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2015,
+      sourceType: 'commonjs'
+    }
+  },
+
+  {
+    files: ['lib/pica_main.js', 'test/**/*.js', 'benchmark/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2018
+    }
+  },
+
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: globals.mocha
+    }
+  }
+]
