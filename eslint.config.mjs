550b393745676df63588155dc7adc8c2867a69e3
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-05-11 07:25:42 +0300

    Cleanup toolchain configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import globals from 'globals'
 export default [
   ...neostandard({
     env: ['browser', 'node'],
+    ts: true,
     ignores: [
       'benchmark/implementations/**',
       'demo/**',
@@ -12,25 +13,6 @@ export default [
   }),
 
   {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2015,
-      sourceType: 'commonjs'
-    },
-    rules: {
-      camelcase: 'off',
-      'one-var': 'off',
-      '@stylistic/no-multiple-empty-lines': 'off',
-      '@stylistic/space-infix-ops': 'off'
-    }
-  },
-
-  {
-    files: ['src/**/*.ts', 'test/**/*.{js,mjs}', 'support/**/*.mjs', 'benchmark/**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module'
-    },
     rules: {
       camelcase: 'off',
       'one-var': 'off',
@@ -42,7 +24,7 @@ export default [
   },
 
   {
-    files: ['test/**/*.{js,mjs}'],
+    files: ['test/**'],
     languageOptions: {
       globals: globals.mocha
     }
